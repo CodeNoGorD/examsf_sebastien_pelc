89ef4b033d182c145d1f7c8ce2ad620010e02083
CodeNoGorD <code.nogord@gmail.com>
2023-07-26 12:32:34 +0200

    navbar

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,22 @@
 import { Tooltip, Toast, Popover } from 'bootstrap'
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+    navbarToggler.classList.toggle("open-navbar-toggler");
+    navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+    if(navbarMenu.classList.contains("open")) {
+        navbarToggler.click();
+    }
+}
