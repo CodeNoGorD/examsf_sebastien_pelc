1b7f123f01ff29bce7262781e1acbbc924a45386
CodeNoGorD <code.nogord@gmail.com>
2023-07-28 08:46:20 +0200

    user crud and authenticator

---
+++ src/Form/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('firstname')
+            ->add('email')
+            ->add('picture')
+            ->add('password')
+            ->add('sector')
+            ->add('contract')
+            ->add('dateContract')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
