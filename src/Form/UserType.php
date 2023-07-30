ee8c0a98fd269b56d6fd0ab4ba3988e9ab47c808
CodeNoGorD <code.nogord@gmail.com>
2023-07-30 23:46:21 +0200

    jeton csrf

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -81,6 +81,7 @@ class UserType extends AbstractType
         $resolver->setDefaults([
             'data_class' => User::class,
             'required' => false,
+            'csrf_protection' => true,
         ]);
     }
 }
